afa3f2ed9aa9f60e833d78a5ff07b4fb8b586358
yuvaldd9 <yuvaldd9@gmail.com>
2020-12-15 19:38:48 +0200

    Upload

---
+++ Onion_Router/Automatic_Execution/r3_auto.py
@@ -0,0 +1,10 @@
+import os
+
+cmd = 'C:\Users\yuval\Desktop\odidion\odidion\Onion_Router\onion_router.py %s %s'%('r3', '50003')
+os_cmd = 'cmd /k \"%s\"'%(cmd,)
+
+os.chdir("C:\Users\yuval\Desktop\odidion\odidion\Onion_Router")
+os.system(os_cmd)
+    
+
+    
